bb9872e3f144b81831e802b7d33af0d5dd4b40c9
Arka Sheikh <skarka90@gmail.com>
2024-01-03 17:32:43 +0530

    done upto special offer section

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,19 @@
+const ServiceCard = ({ service }) => {
+  const { imgURL, label, subtext } = service;
+
+  return (
+    <div className="sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 justify-center items-center bg-coral-red rounded-full flex">
+        <img src={imgURL} alt="label" width={24} height={24} />
+      </div>
+      <h3 className="mt-5 font-palanquin text-3xl font-bold leading-normal">
+        {label}
+      </h3>
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+        {subtext}
+      </p>
+    </div>
+  );
+};
+
+export default ServiceCard;
